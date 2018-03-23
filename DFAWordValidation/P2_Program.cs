0c253de2bd9daf469ee8806cc3efae347c53ccae
AntonDellua <antondellua@gmail.com>
2018-03-22 22:20:25 -0600

    DFA Word Validator and Language Iteration Word Validator added

---
+++ DFAWordValidation/P2_Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*Author:
+ * LUIS ALBERTO ANTON DELGADILLO - ANTON DELLUA - antondellua@gmail.com
+ */
+
+namespace Lenguajes_P2
+{
+    class Program
+    {
+        //Alphabet global variable
+        static char[] alphabet = new char[] { };
+        //Number of states global var
+        static int states;
+        //Array of final states
+        static List<int> finalStates = new List<int>();
+        //Bidimensional array of transitions
+        static int[,] transitions;
+        //Word variable
+        static string word;
+
+        static void Main(string[] args)
+        {
+            //Greet the user and indicate him/her to capture the alphabet
+            Console.WriteLine("DFA Validation, Luis Alberto Anton Delgadillo... Welcome!!! First you need to define the alphabet of symbols:");
+            Console.WriteLine("Introduce all symbols without commas and then press enter");
+            //Capture alphabet
+            string a = Console.ReadLine();
+            //Convert string to char array
+            alphabet = a.ToCharArray();
+            //Clear screen and ask for number of states (q) in the DFA
+            Console.Clear();
+            Console.WriteLine("INPUT VALIDATION: NO");
+            Console.WriteLine("Enter the number of states in the FDA and then press enter. Later, for each state you will have to capture the transition.");
+            //Capture number of states
+            string b = Console.ReadLine();
+            states = Int32.Parse(b);
+            //Now the user is going to type the number of the states that are final
+            Console.Clear();
+            Console.WriteLine("INPUT VALIDATION: NO");
+            Console.WriteLine("Nice, now type the numbers of the states that are final, for each number press enter, to exit type -exit");
+            Console.WriteLine("REMEMBER, the states go from 0 to the number of states you typed -1");
+            //
+            string x = "";
+            int y;
+            do
+            {
+                x = Console.ReadLine();
+                if (x != "-exit")
+                {
+                    y = Int32.Parse(x);
+                    if (y > states) Console.WriteLine("This is not a valid number, try again");
+                    else finalStates.Add(y);
+                }
+            } while (x != "-exit");
+            //Delete duplicates from final states and alphabet
+            finalStates = finalStates.Distinct().ToList();
+            alphabet = alphabet.Distinct().ToArray();
+            //Prompt the user to capture the transitions for each state and alphabet letter
+            Console.Clear();
+            Console.WriteLine("INPUT VALIDATION: NO");
+            Console.WriteLine("Now it's time to fill the transitions, for each state * letter of alphabet");
+            transitions = new int[states, alphabet.Length];
+            for (int i=0; i < states; i++)
+            {
+                int j = 0;
+                foreach(char c in alphabet)
+                {
+                    Console.Write("d(q" + i + "," + c + ") = ");
+                    transitions[i,j] = Int32.Parse(Console.ReadLine());
+                    j++;
+                }
+            }
+            //It's time to capture the words and validate
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("INPUT VALIDATION: YES");
+                Console.WriteLine("Write a word to validate with the DFA");
+                word = Console.ReadLine();
+                if (!AlphabetValidation(word.ToCharArray())) Console.WriteLine("The word does not belong in the Alphabet");
+                else
+                {
+                    //Matrix state iterator
+                    int ite = 0;
+                    foreach (char c in word)
+                    {
+                        //Matrix alphabet iterator
+                        int w = 0;
+                        while (alphabet[w] != c) w++;
+                        Console.Write("d(q" + ite + "," + c + ") = q");
+                        ite = transitions[ite,w];
+                        Console.WriteLine(ite);
+                    }
+                    if (finalStates.Contains(ite)) Console.WriteLine("The state q" + ite + " is part of the Final States, therefore, the word is ACCEPTED!");
+                    else Console.WriteLine("The state q" + ite + " is not part of the Final States, therefore, the word is REJECTED!");
+                }
+                Console.WriteLine("Continue? Yes/No");
+                word = Console.ReadLine();
+                if (word == "No") word = "-exit";
+            } while (word != "-exit");
+        }
+
+        //Method to validate if a word is part of the alphabet
+        static bool AlphabetValidation(char[] word)
+        {
+            //for each char in the word typed
+            foreach (char c in word)
+            {
+                int i = 0;
+                //Check if the char is equal to anyone of the chars or symbols in alphabet
+                foreach (char ch in alphabet)
+                {
+                    if (ch == c)
+                    {
+                        i = 1;
+                    }
+                }
+                //if none is equal, return false
+                if (i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ValidateLanguageIteration/P1_Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*Author:
+ * LUIS ALBERTO ANTON DELGADILLO - ANTON DELLUA - antondellua@gmail.com
+ */
+
+namespace LenguajesFormales_Practica1
+{
+    class Program
+    {
+        //Alphabet global variable
+        static char[] alphabet = new char[] { };
+        //Language global List
+        static List<string> language = new List<string>();
+        
+        //Main Program
+        static void Main(string[] args)
+        {
+            //Greet the user and indicate him/her to capture the alphabet
+            Console.WriteLine("Word-Language Validation, Luis Alberto Anton Delgadillo... Welcome!!! First you need to define the alphabet of symbols:");
+            Console.WriteLine("Introduce all symbols without commas and then press enter");
+            //Capture alphabet
+            string a = Console.ReadLine();
+            //Convert string to char array
+            alphabet = a.ToCharArray();
+            //Tell the user he´s going to capture the language
+            Console.Clear();
+            Console.WriteLine("Great! Now introduce the language, for each word press enter, once you're done type -exit and press enter");
+            //Loop to store language
+            string x = "";
+            //While the word typed is different from -exit it validates it with the alphabet and stores it at the language
+            do
+            {
+                x = Console.ReadLine();
+                if (x != "-exit")
+                {
+                    var temp = x.ToCharArray();
+                    //If word belongs to alphabet, store it
+                    if (AlphabetValidation(temp))
+                    {
+                        language.Add(x);
+                    }
+                    //else, throw error
+                    else Console.WriteLine("Word does not belong to Alphabet, try another one");
+                }
+                /*else if (language.Count == 0)
+                {
+                    Console.WriteLine("Language must contain at least one word");
+                    x = "";
+                }*/
+            } while (x != "-exit");
+            //Now we´re going to check n number of words typed by the user in the language concatenations
+           
+            string word = "";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Now introduce one word to see if it belongs to some iteration of the language:");
+                word = Console.ReadLine();
+                char[] temp = word.ToCharArray();
+                //Check if word belongs to alphabet
+                if (!AlphabetValidation(temp))
+                {
+                    Console.WriteLine("The word does not belong in the Alphabet");
+                }
+                //Check if word does not belong in alphabet
+                else if (!LanguageValidation(word))
+                {
+                    Console.WriteLine("The word does not belong in the Language");
+                }
+                //Else, word belongs to language
+                else
+                {
+                    Console.WriteLine("The word belongs to the language!!!");
+                }
+                Console.WriteLine("Continue? Yes/No");
+                word = Console.ReadLine();
+                if (word == "No") word = "-exit";
+            } while (word != "-exit");
+        }
+
+        //Method to validate if a word id part of the language
+        static bool LanguageValidation(string word)
+        {
+            //Control variable to break loop if word does not belong
+            int control = 0;
+            //while there is a word to compare, loop
+            do
+            {
+                control = 0;
+                //compare every word in the language with the given word
+                foreach (string w in language)
+                {
+                    //Here we evaluate only the words that are equal or shorter than the given word
+                    //If the evaluated word in the language is part of the beginning of the given word, remove it and shorten the given word, then reevaluate until it's empty or proven false
+
+                    if (w.Length <= word.Length)
+                    {
+                        if (word.StartsWith(w))
+                        {
+                            word = word.Remove(0, w.Length);
+                            control = 1;
+                        }
+                    }
+                }
+                if (control == 0) return false;
+            } while (word.Length > 0);
+            //If the program makes it all the way here is because the give word is now empty and the control var was never 0, hence, true.
+            return true;
+        }
+
+        //Method to get length of shortest word of the language
+        static int GetShortestWord(List<string> language)
+        {
+            int n = language[0].Length;
+            foreach (string s in language)
+            {
+                if (s.Length < n) n = s.Length;
+            }
+            return n;
+        }
+
+        //Method to validate if a word is part of the alphabet
+        static bool AlphabetValidation(char[] word)
+        {
+            //for each char in the word typed
+            foreach(char c in word)
+            {
+                int i = 0;
+                //Check if the char is equal to anyone of the chars or symbols in alphabet
+                foreach(char ch in alphabet)
+                {
+                    if (ch == c)
+                    {
+                        i = 1;
+                    }
+                }
+                //if none is equal, return false
+                if (i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
